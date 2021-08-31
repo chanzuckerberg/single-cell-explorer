c0698dcf7ee62424e0ee6f8adda1f1529e515ede
Mim Hastie <mim@clevercanary.com>
2021-08-31 15:18:47 -0700

    Typed dataset drawer. (#22) (#29)

--- client/src/components/infoDrawer/infoDrawer.tsx
+++ client/src/components/infoDrawer/infoDrawer.tsx
@@ -1,7 +1,9 @@
+// Core dependencies
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import { Drawer } from "@blueprintjs/core";
 
+// App dependencies
 import InfoFormat from "./infoFormat";
 import { selectableCategoryNames } from "../../util/stateManager/controlsHelpers";
 
@@ -11,10 +13,6 @@ import { selectableCategoryNames } from "../../util/stateManager/controlsHelpers
   collection: ((state as any).collections as any)?.collection,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any --- FIXME: disabled temporarily on migrate to TS.
   schema: (state as any).annoMatrix.schema,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any --- FIXME: disabled temporarily on migrate to TS.
-  datasetTitle: (state as any).config?.displayNames?.dataset ?? "",
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any --- FIXME: disabled temporarily on migrate to TS.
-  aboutURL: (state as any).config?.links?.["about-dataset"],
   // eslint-disable-next-line @typescript-eslint/no-explicit-any --- FIXME: disabled temporarily on migrate to TS.
   isOpen: (state as any).controls.datasetDrawer,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any --- FIXME: disabled temporarily on migrate to TS.
@@ -29,17 +27,12 @@ class InfoDrawer extends PureComponent {
     dispatch({ type: "toggle dataset drawer" });
   };
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types --- FIXME: disabled temporarily on migrate to TS.
-  render() {
+  render(): JSX.Element {
     const {
       // @ts-expect-error ts-migrate(2339) FIXME: Property 'collection' does not exist on type 'Readon... Remove this comment to see the full error message
       collection,
       // @ts-expect-error ts-migrate(2339) FIXME: Property 'position' does not exist on type 'Readon... Remove this comment to see the full error message
       position,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'aboutURL' does not exist on type 'Readon... Remove this comment to see the full error message
-      aboutURL,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'datasetTitle' does not exist on type 'Re... Remove this comment to see the full error message
-      datasetTitle,
       // @ts-expect-error ts-migrate(2339) FIXME: Property 'schema' does not exist on type 'Readonly... Remove this comment to see the full error message
       schema,
       // @ts-expect-error ts-migrate(2339) FIXME: Property 'isOpen' does not exist on type 'Readonly... Remove this comment to see the full error message
@@ -52,8 +45,7 @@ class InfoDrawer extends PureComponent {
     const allCategoryNames = selectableCategoryNames(schema).sort();
     const singleValueCategories = new Map();
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any --- FIXME: disabled temporarily on migrate to TS.
-    allCategoryNames.forEach((catName: any) => {
+    allCategoryNames.forEach((catName: string) => {
       const isUserAnno = schema?.annotations?.obsByName[catName]?.writable;
       const colSchema = schema.annotations.obsByName[catName];
       if (!isUserAnno && colSchema.categories?.length === 1) {
@@ -62,7 +54,13 @@ class InfoDrawer extends PureComponent {
     });
 
     return (
-      <Drawer onClose={this.handleClose} size={480} {...{ isOpen, position }}>
+      <Drawer
+        isCloseButtonShown={false}
+        title="Dataset Overview"
+        size={480}
+        onClose={this.handleClose}
+        {...{ isOpen, position }}
+      >
         <InfoFormat
           {...{
             collection,
