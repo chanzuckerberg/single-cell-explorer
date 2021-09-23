e807dcade22db6699a11e6d6d054bedb78822d63
Timmy Huang <tihuan@users.noreply.github.com>
2021-09-23 16:09:03 -0700

    chore: Add types for rrecognized version commit (#81)

--- client/src/components/util/version.ts
+++ client/src/components/util/version.ts
@@ -1,16 +1,17 @@
+import { DataPortalProps } from "../../common/types/entities";
+
 const VERSION_ONES = ["1.0.0", "1.1.0"];
 const VERSION_TWOS = ["2.0.0"];
 
-export function checkValidVersion(corporaProps: any) {
+export function checkValidVersion(corporaProps: DataPortalProps): boolean {
   if (!corporaProps) return false;
 
-  // eslint-disable-next-line @typescript-eslint/naming-convention -- prop from BE
-  const { version, schema_version } = corporaProps;
+  const { version, schema_version: schemaVersion } = corporaProps;
 
   const isValidVersionOne = VERSION_ONES.includes(
     version?.corpora_schema_version
   );
-  const isValidVersionTwo = VERSION_TWOS.includes(schema_version);
+  const isValidVersionTwo = VERSION_TWOS.includes(schemaVersion);
 
   return isValidVersionOne || isValidVersionTwo;
 }
