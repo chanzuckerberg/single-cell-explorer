7e8bbe79330f5e3f8a764160b952c58f7ed6bf9e
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2021-09-29 12:35:56 -0400

    More attempts

--- server/eb/app.py
+++ server/eb/app.py
@@ -129,7 +129,7 @@ class WSGIServer(Server):
     @staticmethod
     def get_csp_hashes(app, app_config):
         script_hashes = WSGIServer.load_static_csp_hashes(app)
-        script_hashes += WSGIServer.compute_inline_csp_hashes(app, app_config)
+        # script_hashes += WSGIServer.compute_inline_csp_hashes(app, app_config)
         return script_hashes
 
 
