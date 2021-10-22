d98dc6264ceec2a1f2431cb66eb73b9c1d27b764
MillenniumFalconMechanic <mim@clevercanary.com>
2021-10-21 22:42:16 -0700

    Updated load indicator to match skeleton. (#139)

--- client/src/components/leftSidebar/leftSidebarSkeleton.tsx
+++ client/src/components/leftSidebar/leftSidebarSkeleton.tsx
@@ -1,6 +1,6 @@
 /* Core dependencies */
 import { SKELETON } from "@blueprintjs/core/lib/esnext/common/classes";
-import React from "react";
+import React, { CSSProperties } from "react";
 
 /* App dependencies */
 import Logo from "../framework/logo";
@@ -8,7 +8,15 @@ import Title from "../framework/title";
 
 /* Styles */
 import { STYLE_LEFT_SIDEBAR } from ".";
+import StillLoading from "../brushableHistogram/loading";
+import { StillLoading as CategoryLoading } from "../categorical/category";
 import { STYLE_TOP_LEFT_LOGO_AND_TITLE } from "./topLeftLogoAndTitle";
+
+const STYLE_SUPER_CATEGORY: CSSProperties = {
+  height: 22,
+  margin: "8px 0",
+  width: 160,
+};
 
 /**
  * Skeleton of left side bar, to be displayed during data load.
@@ -33,19 +41,22 @@ function LeftSidebarSkeleton(): JSX.Element {
         <div style={{ height: 30, width: 30 }} className={SKELETON} />
       </div>
       {/* Categorical */}
-      <div style={{ padding: 8 }}>
+      <div style={{ padding: 10 }}>
+        <div style={STYLE_SUPER_CATEGORY} className={SKELETON} />
         {[...Array(10).keys()].map((i) => (
-          <div
-            key={i}
-            style={{ height: 30, marginBottom: 4 }}
-            className={SKELETON}
-          />
+          <CategoryLoading key={i} />
         ))}
       </div>
       {/* Continuous */}
-      {[...Array(2).keys()].map((i) => (
-        <div key={i} style={{ height: 211 }} className={SKELETON} />
-      ))}
+      <div>
+        <div
+          style={{ ...STYLE_SUPER_CATEGORY, marginLeft: 10 }}
+          className={SKELETON}
+        />
+        {[...Array(2).keys()].map((i) => (
+          <StillLoading key={i} />
+        ))}
+      </div>
     </div>
   );
 }
