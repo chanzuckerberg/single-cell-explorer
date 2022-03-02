f9a8fc46a081c0e79da046f1e7ef67f05d5d442a
Bruce Martin <bruce@chanzuckerberg.com>
2022-03-02 07:29:02 -0800

    diffex PDU support (#228)
    * initial commit of diffex pdu support
    
    * lint
    
    * fix typo in type signature
    
    * more lint
    
    * another typo in typing
    
    * remove unsupported python feature use
    
    * add additional comments to entry functions
    
    * fix bug in test shuffle
    
    * PR review fixes
    
    * add top-level magic number; added end-to-end test scripts
    
    * lint
    
    * refine comments and docs

---
+++ server/common/diffexpdu/__init__.py
@@ -0,0 +1,4 @@
+from .postingslist import deflate_postings_lists, inflate_postings_lists
+from .diffexpdu import DiffExArguments
+
+__all__ = ["deflate_postings_lists", "inflate_postings_lists", "DiffExArguments"]
