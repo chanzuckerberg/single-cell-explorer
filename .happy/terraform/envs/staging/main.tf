f5993b9272985275a096ebe8bf810c0b02ee2bd6
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-05-11 15:10:02 -0400

    chore: happy terraform staging (#256)
    * staging
    
    * Disable migrations

---
+++ .happy/terraform/envs/staging/main.tf
@@ -0,0 +1,18 @@
+module stack {
+  source                       = "./modules/ecs-stack"
+  aws_account_id               = var.aws_account_id
+  aws_role                     = var.aws_role
+  happymeta_                   = var.happymeta_
+  happy_config_secret          = var.happy_config_secret
+  image_tag                    = var.image_tag
+  priority                     = var.priority
+  stack_name                   = var.stack_name
+  deployment_stage             = "staging"
+  delete_protected             = false
+  require_okta                 = false
+  stack_prefix                 = "/${var.stack_name}"
+  batch_container_memory_limit = 28000
+  memory                       = 50000
+
+  wait_for_steady_state        = var.wait_for_steady_state
+}
