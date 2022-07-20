84262e505a7c63a0e4bd681817c777c1ef3ab859
Siena Cizdziel <scizdziel@CZIMACOS4051.local>
2022-07-20 16:04:33 -0700

    fixing styling issues

--- client/src/components/geneExpression/geneInfo/geneInfo.tsx
+++ client/src/components/geneExpression/geneInfo/geneInfo.tsx
@@ -83,6 +83,7 @@ class GeneInfo extends React.PureComponent<Props, State> {
             level === "top"
               ? globals.bottomToolbarGutter * 2
               : globals.bottomToolbarGutter,
+          left: globals.leftSidebarWidth + globals.scatterplotMarginLeft,
         }}
         id="geneinfo_wrapper"
       >
