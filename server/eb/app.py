268f42ac7bb9839b6a627cfcba3117781e75a221
atarashansky <atarashansky@CZIMACOS3990.local>
2022-07-22 09:22:07 -0400

    remove testing lines

--- server/eb/app.py
+++ server/eb/app.py
@@ -7,9 +7,6 @@ from flask import json
 import logging
 from flask_talisman import Talisman
 from flask_cors import CORS
-from flask_compress import Compress
-
-compress = Compress()
 
 if os.path.isdir("/opt/python/log"):
     # This is the standard location where Amazon EC2 instances store the application logs.
@@ -176,8 +173,6 @@ try:
 
     server = WSGIServer(app_config)
     debug = False
-    compress.init_app(server.app)
-
     application = server.app
 
 except Exception:
