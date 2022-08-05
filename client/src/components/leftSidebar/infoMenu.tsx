3a03e6f30f4fee9ab2e2813d963fb5841d0e6cdd
ashin-czi <ashin@contractor.chanzuckerberg.com>
2022-08-05 13:18:29 -0400

    chore: changing info menu about text

--- client/src/components/leftSidebar/infoMenu.tsx
+++ client/src/components/leftSidebar/infoMenu.tsx
@@ -35,7 +35,7 @@ const InformationMenu = React.memo<Props>((props): JSX.Element => {
           <MenuItem
             icon={<Icon icon={CXGIconNames.ABOUT} />}
             popoverProps={{ openOnTargetFocus: false }}
-            text="About CZ CELLxGENE"
+            text="About CZ CELLxGENE Discover"
           >
             {tosURL && (
               <MenuItem
