4316fc4a565b63ee4a970c03762b316d8c8a6627
atarashansky <atarashansky@CZIMACOS3990.hsd1.ma.comcast.net>
2022-09-27 11:00:08 -0400

    add hotkey component

---
+++ client/src/components/hotkeys/index.tsx
@@ -0,0 +1,64 @@
+import { useHotkeys, InputGroup } from "@blueprintjs/core";
+import React, { FC, useMemo } from "react";
+import { connect } from "react-redux";
+import { AppDispatch } from "../../reducers";
+import { track } from "../../analytics";
+import { EVENTS } from "../../analytics/events";
+
+interface DispatchProps {
+  undo: () => void;
+  redo: () => void;
+}
+type Props = DispatchProps;
+
+const mapDispatchToProps = (dispatch: AppDispatch): DispatchProps => ({
+  undo: () => {
+    track(EVENTS.EXPLORER_UNDO_BUTTON_CLICKED);
+    dispatch({ type: "@@undoable/undo" });
+  },
+  redo: () => {
+    track(EVENTS.EXPLORER_REDO_BUTTON_CLICKED);
+    dispatch({ type: "@@undoable/redo" });
+  },
+});
+
+const GlobalHotkeys: FC<Props> = ({ undo, redo }) => {
+  const hotkeys = useMemo(
+    () => [
+      {
+        combo: "CTRL+Z",
+        global: true,
+        label: "Undo.",
+        onKeyDown: () => {
+          undo();
+        },
+      },
+      {
+        combo: "CTRL+SHIFT+Z",
+        global: true,
+        label: "Redo.",
+        onKeyDown: () => {
+          redo();
+        },
+      },
+    ],
+    []
+  );
+  const { handleKeyDown, handleKeyUp } = useHotkeys(hotkeys);
+
+  return (
+    <div
+      role="tab"
+      tabIndex={0}
+      onKeyDown={handleKeyDown}
+      onKeyUp={handleKeyUp}
+      style={{
+        display: "none",
+      }}
+    >
+      <InputGroup />
+    </div>
+  );
+};
+
+export default connect(null, mapDispatchToProps)(GlobalHotkeys);
