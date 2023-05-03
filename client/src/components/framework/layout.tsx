98303266b78c6ef862430e37d6ddfa11c0931a1f
ashin-czi <ashin@contractor.chanzuckerberg.com>
2023-05-03 14:12:32 -0400

    indenting

--- client/src/components/framework/layout.tsx
+++ client/src/components/framework/layout.tsx
@@ -32,76 +32,74 @@ const Layout: React.FC<Props> = (props) => {
     graphComponent = renderGraph(viewportRef);
   }
   return (
-    <>
+    <div
+      style={{
+        display: "grid",
+        paddingTop: !datasetMetadataError ? globals.HEADER_HEIGHT_PX : 0,
+        gridTemplateColumns: `
+          [left-sidebar-start] ${globals.leftSidebarWidth + 1}px
+          [left-sidebar-end graph-start] auto
+          [graph-end right-sidebar-start] ${
+            globals.rightSidebarWidth + 1
+          }px [right-sidebar-end]
+        `,
+        gridTemplateRows: "[top] auto [bottom]",
+        gridTemplateAreas: "left-sidebar | graph | right-sidebar",
+        columnGap: "0px",
+        justifyItems: "stretch",
+        alignItems: "stretch",
+        height: "inherit",
+        width: "inherit",
+        position: "relative",
+        top: 0,
+        left: 0,
+        minWidth: "1240px",
+      }}
+    >
       <div
         style={{
-          display: "grid",
-          paddingTop: !datasetMetadataError ? globals.HEADER_HEIGHT_PX : 0,
-          gridTemplateColumns: `
-        [left-sidebar-start] ${globals.leftSidebarWidth + 1}px
-        [left-sidebar-end graph-start] auto
-        [graph-end right-sidebar-start] ${
-          globals.rightSidebarWidth + 1
-        }px [right-sidebar-end]
-      `,
-          gridTemplateRows: "[top] auto [bottom]",
-          gridTemplateAreas: "left-sidebar | graph | right-sidebar",
-          columnGap: "0px",
-          justifyItems: "stretch",
-          alignItems: "stretch",
-          height: "inherit",
-          width: "inherit",
+          gridArea: "top / left-sidebar-start / bottom / left-sidebar-end",
           position: "relative",
-          top: 0,
-          left: 0,
-          minWidth: "1240px",
+          height: "inherit",
+          overflowY: "auto",
+          paddingBottom: isBannerOpen ? `${BANNER_HEIGHT_PX}px` : 0,
         }}
       >
-        <div
-          style={{
-            gridArea: "top / left-sidebar-start / bottom / left-sidebar-end",
-            position: "relative",
-            height: "inherit",
-            overflowY: "auto",
-            paddingBottom: isBannerOpen ? `${BANNER_HEIGHT_PX}px` : 0,
-          }}
-        >
-          {leftSidebar}
-        </div>
-        <div
-          style={{
-            zIndex: 0,
-            gridArea: "top / graph-start / bottom / graph-end",
-            position: "relative",
-            height: "inherit",
-          }}
-          ref={(ref) => {
-            setViewportRef(ref);
-          }}
-        >
-          {graphComponent}
-          <Controls bottom={isBannerOpen ? BANNER_HEIGHT_PX : 0}>
-            <DatasetSelector />
-          </Controls>
-        </div>
-        <div
-          style={{
-            gridArea: "top / right-sidebar-start / bottom / right-sidebar-end",
-            position: "relative",
-            height: "inherit",
-            overflowY: "auto",
-            paddingBottom: isBannerOpen ? `${BANNER_HEIGHT_PX}px` : 0,
-          }}
-        >
-          {/* The below conditional is required because the right sidebar initializes as function for some reason...*/}
-          {!(rightSidebar instanceof Function) && rightSidebar}
-        </div>
-        <BottomBanner
-          includeSurveyLink={false}
-          setIsBannerOpen={setIsBannerOpen}
-        />
+        {leftSidebar}
       </div>
-    </>
+      <div
+        style={{
+          zIndex: 0,
+          gridArea: "top / graph-start / bottom / graph-end",
+          position: "relative",
+          height: "inherit",
+        }}
+        ref={(ref) => {
+          setViewportRef(ref);
+        }}
+      >
+        {graphComponent}
+        <Controls bottom={isBannerOpen ? BANNER_HEIGHT_PX : 0}>
+          <DatasetSelector />
+        </Controls>
+      </div>
+      <div
+        style={{
+          gridArea: "top / right-sidebar-start / bottom / right-sidebar-end",
+          position: "relative",
+          height: "inherit",
+          overflowY: "auto",
+          paddingBottom: isBannerOpen ? `${BANNER_HEIGHT_PX}px` : 0,
+        }}
+      >
+        {/* The below conditional is required because the right sidebar initializes as function for some reason...*/}
+        {!(rightSidebar instanceof Function) && rightSidebar}
+      </div>
+      <BottomBanner
+        includeSurveyLink={false}
+        setIsBannerOpen={setIsBannerOpen}
+      />
+    </div>
   );
 };
 
