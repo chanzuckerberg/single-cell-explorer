1f3c0c80e993e30ebcbe6f98c913effa5bd2ffad
Andrew Tolopko <atolopko@chanzuckerberg.com>
2023-10-19 10:06:33 -0400

    add usage instructions

--- scripts/patch-explorer-to-find-portal-rdev-stack.sh
+++ scripts/patch-explorer-to-find-portal-rdev-stack.sh
@@ -1,2 +1,8 @@
+# This script applies a (temporary) patch to Explorer so that the RDev
+# stack can find the associated Portal RDev stack and successfully
+# make Portal API calls during manual testing.
+#
+# Usage patch-explorer-to-find-portal-rdev-stack.sh
+
 cd $(git rev-parse --show-toplevel)
 sed -e "s/STACK_NAME/${1}/g" scripts/explorer-with-portal-rdev.patch | patch
