65d3ce1919dd8d60db8762f61aa52d0f96862156
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2024-01-23 14:04:47 -0800

    test: increase timeout

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -73,6 +73,8 @@ export default defineConfig({
   },
   maxFailures: process.env.CI ? CICD_MAX_FAILURE : undefined,
 
+  timeout: 2.5 * 60 * 1000, // 2.5 minutes
+
   /* Configure projects for major browsers */
   projects: [
     {
