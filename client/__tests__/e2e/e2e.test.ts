99afe27d20036e81a579ef73ca7eb02518345cf8
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2024-02-05 14:17:23 -0800

    lint

--- client/__tests__/e2e/e2e.test.ts
+++ client/__tests__/e2e/e2e.test.ts
@@ -42,8 +42,6 @@ import {
   removeGeneInfo,
   addGeneToSearch,
   assertGeneInfoDoesNotExist,
-  keyboardUndo,
-  keyboardRedo,
   waitUntilNoSkeletonDetected,
   checkGenesetDescription,
   assertUndoRedo,
