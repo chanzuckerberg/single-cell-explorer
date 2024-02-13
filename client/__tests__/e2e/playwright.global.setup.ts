66741b681dd21e57147df3c88804403bb63c8603
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2024-02-13 09:17:20 -0800

    test: add methods to fail tests on error logs and uncaught errors (#750)

--- client/__tests__/e2e/playwright.global.setup.ts
+++ client/__tests__/e2e/playwright.global.setup.ts
@@ -42,6 +42,15 @@ const setup = async ({ page }: { page: Page }) => {
       },
     });
   });
+
+  page.on("console", (message) => {
+    if (message.type() === "error") {
+      throw new Error(`CLIENT SIDE ERROR: ${  message.text()}`);
+    }
+  });
+  page.on("pageerror", (error) => {
+    throw new Error(`UNCAUGHT CLIENT ERROR: ${  error}`);
+  });
 };
 
 export default setup;
