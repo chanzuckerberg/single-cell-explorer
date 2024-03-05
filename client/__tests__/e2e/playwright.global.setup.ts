2178d7e3459d79d86720ac1599d07dbdfb449f0c
Timmy Huang <thuang@chanzuckerberg.com>
2024-03-05 10:29:32 -0800

    chore: better error message

--- client/__tests__/e2e/playwright.global.setup.ts
+++ client/__tests__/e2e/playwright.global.setup.ts
@@ -45,7 +45,7 @@ const setup = async ({ page }: { page: Page }) => {
 
   page.on("console", (message) => {
     if (message.type() === "error") {
-      throw new Error(`CLIENT SIDE ERROR: ${  message.text()}`);
+      throw new Error(`CLIENT SIDE ERROR: ${JSON.stringify(message)}`)
     }
   });
   page.on("pageerror", (error) => {
