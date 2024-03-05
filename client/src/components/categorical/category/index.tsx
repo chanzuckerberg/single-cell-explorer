bd213e754f8a1b278082001679a2835a6ed5cd99
atarashansky <atarashansky@chanzuckerberg.com>
2024-03-05 15:20:10 -0800

    gate changes

--- client/src/components/categorical/category/index.tsx
+++ client/src/components/categorical/category/index.tsx
@@ -612,6 +612,7 @@ const CategoryRender = React.memo(
                 colorData={colorData}
                 colorTable={colorTable}
                 colorMode={colorMode}
+                isCellGuideCxg={isCellGuideCxg}
               />
             ) : null
           }
@@ -630,6 +631,7 @@ interface CategoryValueListProps {
   colorData: any;
   colorTable: any;
   colorMode: string;
+  isCellGuideCxg: boolean;
 }
 const CategoryValueList = React.memo(
   ({
@@ -641,52 +643,56 @@ const CategoryValueList = React.memo(
     colorData,
     colorTable,
     colorMode,
+    isCellGuideCxg,
   }: CategoryValueListProps) => {
-    const tuples = [...categorySummary.categoryValueIndices].filter(
-      ([value, index]) => categorySummary.categoryValueCounts[index] > 0
-    );
+    let tuples = [...categorySummary.categoryValueIndices];
+    if (isCellGuideCxg) {
+      tuples = tuples.filter(
+        ([value, index]) => categorySummary.categoryValueCounts[index] > 0
+      );
 
-    if (
-      colorMode === "color by continuous metadata" ||
-      colorMode === "color by expression" ||
-      colorMode === "color by geneset mean expression"
-    ) {
-      const categoryDataArray = categoryData.col(metadataField).asArray();
-      const colorDataArray = colorData.icol(0).asArray();
-      const categoryColorMap = new Map();
-      categoryDataArray.forEach((category: string, index: number) => {
-        if (!categoryColorMap.has(category)) {
-          categoryColorMap.set(category, { sum: 0, count: 0 });
-        }
-        const colorValue = colorDataArray[index];
-        const categoryColor = categoryColorMap.get(category);
-        categoryColor.sum += colorValue;
-        categoryColor.count += 1;
-      });
+      if (
+        colorMode === "color by continuous metadata" ||
+        colorMode === "color by expression" ||
+        colorMode === "color by geneset mean expression"
+      ) {
+        const categoryDataArray = categoryData.col(metadataField).asArray();
+        const colorDataArray = colorData.icol(0).asArray();
+        const categoryColorMap = new Map();
+        categoryDataArray.forEach((category: string, index: number) => {
+          if (!categoryColorMap.has(category)) {
+            categoryColorMap.set(category, { sum: 0, count: 0 });
+          }
+          const colorValue = colorDataArray[index];
+          const categoryColor = categoryColorMap.get(category);
+          categoryColor.sum += colorValue;
+          categoryColor.count += 1;
+        });
 
-      const categoryAverageColor = new Map();
-      categoryColorMap.forEach((value, key) => {
-        categoryAverageColor.set(key, value.sum / value.count);
-      });
-      tuples.sort((a, b) => {
-        const colorA = categoryAverageColor.get(a[0]);
-        const colorB = categoryAverageColor.get(b[0]);
-        return colorB - colorA;
-      });
+        const categoryAverageColor = new Map();
+        categoryColorMap.forEach((value, key) => {
+          categoryAverageColor.set(key, value.sum / value.count);
+        });
+        tuples.sort((a, b) => {
+          const colorA = categoryAverageColor.get(a[0]);
+          const colorB = categoryAverageColor.get(b[0]);
+          return colorB - colorA;
+        });
+      }
+      // (alec): This is to sort the categories by the number of cells in each category
+      // Disabling for now, but it could be useful.
+      // else {
+      //   tuples.sort(
+      //     (a, b) =>
+      //       categorySummary.categoryValueCounts[b[1]] -
+      //       categorySummary.categoryValueCounts[a[1]]
+      //   );
+      // }
+      /*
+      Render the value list.  If this is a user annotation, we use a flipper
+      animation, if read-only, we don't bother and save a few bits of perf.
+      */
     }
-    // (alec): This is to sort the categories by the number of cells in each category
-    // Disabling for now, but it could be useful.
-    // else {
-    //   tuples.sort(
-    //     (a, b) =>
-    //       categorySummary.categoryValueCounts[b[1]] -
-    //       categorySummary.categoryValueCounts[a[1]]
-    //   );
-    // }
-    /*
-    Render the value list.  If this is a user annotation, we use a flipper
-    animation, if read-only, we don't bother and save a few bits of perf.
-    */
     if (!isUserAnno) {
       return (
         <>
