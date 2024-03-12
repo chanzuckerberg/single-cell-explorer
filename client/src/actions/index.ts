a6ac7c6e294712d22af2ff6b32fe7c94a1fe80dd
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2024-03-12 10:30:26 -0700

    fix callsite

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -225,7 +225,7 @@ const doInitialDataLoad = (): ((
       datasetMetadataFetchAndLoad(dispatch, oldPrefix, config);
       // TODO: add logic to ensure this is working for spatial datasets when flag removed
       if (isSpatial) {
-        datasetSpatialMetadataFetchAndLoad(dispatch, oldPrefix);
+        datasetSpatialMetadataFetchAndLoad(dispatch);
       }
       const baseDataUrl = `${globals.API.prefix}${globals.API.version}`;
       const annoMatrix = new AnnoMatrixLoader(baseDataUrl, schema.schema);
