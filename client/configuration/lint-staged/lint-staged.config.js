fe6683e5e16f23735b09e8c7c91ddb309b36d491
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2024-03-26 13:41:17 -0700

    chore: fix latent lint errors (#851)

--- client/configuration/lint-staged/lint-staged.config.js
+++ client/configuration/lint-staged/lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  "*.{js,ts,jsx,tsx}": "eslint --fix",
+  "*.{js,ts,jsx,tsx}": [() => "tsc --project tsconfig.json", "eslint --fix"],
   "src/**/*": "prettier --write --ignore-unknown",
-  "*.{ts,tsx}": "tsc --noEmit",
 };
