7f0bdd69fad28c5ae1c1c451ba1c47c34a0f3112
kaloster <rkalo@contractor.chanzuckerberg.com>
2024-04-04 20:05:29 +0300

    headers

--- server/tests/unit/common/apis/test_api_v3.py
+++ server/tests/unit/common/apis/test_api_v3.py
@@ -739,7 +739,7 @@ class EndPoints(BaseTest):
         for url_base in [self.TEST_URL_BASE]:
             with self.subTest(url_base=url_base):
                 url = f"{url_base}{endpoint}?{query}"
-                header = {"Accept": "application/ojson"}
+                header = {"Content-Type": "application/json"}
                 result = self.client.get(url, headers=header)
                 self.assertEqual(result.status_code, HTTPStatus.OK)
                 self.assertEqual(result.headers["Content-Type"], "application/json")
@@ -760,7 +760,7 @@ class EndPoints(BaseTest):
         for url_base in [self.TEST_UNS_URL_BASE]:
             with self.subTest(url_base=url_base):
                 url = f"{url_base}{endpoint}?{query}"
-                header = {"Accept": "application/ojson"}
+                header = {"Content-Type": "application/json"}
                 result = self.client.get(url, headers=header)
 
                 self.assertEqual(result.status_code, HTTPStatus.OK)
