fe5ff47030db45970a7cbb64e98cef474fa1c965
Ronen <kaloster@users.noreply.github.com>
2024-06-12 12:12:13 -0400

    chore: prod deploy; 06/12/24 (#976)
    Co-authored-by: Nayib Gloria <55710092+nayib-jose-gloria@users.noreply.github.com>

--- server/dataset/dataset.py
+++ server/dataset/dataset.py
@@ -346,7 +346,7 @@ class Dataset(metaclass=ABCMeta):
         Embedding is an ndarray, shape (n_obs, n)., where n is normally 2
         """
 
-        if spatial is not None and ename in "spatial":
+        if spatial and ename in "spatial":
 
             library_id = list(spatial.keys())[0]
             image_properties = spatial[library_id]["image_properties"]
