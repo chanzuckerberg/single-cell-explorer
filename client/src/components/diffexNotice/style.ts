c4dbb37ac9cb64b4ec6b560900a0d6cd2ec088bf
Timmy Huang <tihuan@users.noreply.github.com>
2024-07-10 09:28:34 -0700

    fix: diffexp notif (#1031)
    Co-authored-by: atarashansky <atarashansky@chanzuckerberg.com>
    Co-authored-by: atarashansky <atarashansky@users.noreply.github.com>

---
+++ client/src/components/diffexNotice/style.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import Snackbar from "@material-ui/core/Snackbar";
+import { Alert } from "@material-ui/lab";
+
+export const StyledSnackbar = styled(Snackbar)`
+  width: 423px;
+`;
+
+export const StyledAlert = styled(Alert)`
+  width: 100%;
+  font-size: 13px;
+  font-family: "Inter";
+  font-weight: 400;
+  line-height: 20px;
+`;
