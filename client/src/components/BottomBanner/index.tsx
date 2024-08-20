de849f4b84a04800db9dd4895da68b7ba31d5006
kaloster <kaloster@users.noreply.github.com>
2024-08-20 17:08:53 +0000

    fix: adjust BottomBanner

--- client/src/components/BottomBanner/index.tsx
+++ client/src/components/BottomBanner/index.tsx
@@ -26,47 +26,45 @@ const mapDispatchToProps = (dispatch: AppDispatch) => ({
   dispatch,
 });
 
-const getBannerContent = (surveyLink: string): JSX.Element => (
-  <span>
-    {BOTTOM_BANNER_SURVEY_TEXT}
-    <StyledLink href={surveyLink} target="_blank" rel="noopener">
-      {BOTTOM_BANNER_SURVEY_LINK_TEXT}
-    </StyledLink>
-  </span>
-);
+function BannerContent({ surveyLink }: { surveyLink: string }): JSX.Element {
+  return (
+    <span>
+      {BOTTOM_BANNER_SURVEY_TEXT}
+      <StyledLink href={surveyLink} target="_blank" rel="noopener">
+        {BOTTOM_BANNER_SURVEY_LINK_TEXT}
+      </StyledLink>
+    </span>
+  );
+}
 
-const BottomBanner = memo(
-  ({
-    surveyLink,
-    showBottomBanner,
-    dispatch,
-  }: BottomBannerProps): JSX.Element | null => {
-    const setBottomBannerLastClosedTime = () => {
-      dispatch({
-        type: "update bottom banner last closed time",
-        time: Date.now(),
-      });
-    };
+function BottomBanner({
+  surveyLink,
+  showBottomBanner,
+  dispatch,
+}: BottomBannerProps): JSX.Element | null {
+  const setBottomBannerLastClosedTime = () => {
+    dispatch({
+      type: "update bottom banner last closed time",
+      time: Date.now(),
+    });
+  };
 
-    if (!showBottomBanner) return null;
+  if (!showBottomBanner) return null;
 
-    return (
-      <>
-        <StyledBottomBannerWrapper
-          id={BOTTOM_BANNER_ID}
-          data-testid={BOTTOM_BANNER_ID}
-        >
-          <StyledBanner
-            dismissible
-            sdsType="primary"
-            onClose={setBottomBannerLastClosedTime}
-          >
-            {getBannerContent(surveyLink)}
-          </StyledBanner>
-        </StyledBottomBannerWrapper>
-      </>
-    );
-  }
-);
+  return (
+    <StyledBottomBannerWrapper
+      id={BOTTOM_BANNER_ID}
+      data-testid={BOTTOM_BANNER_ID}
+    >
+      <StyledBanner
+        dismissible
+        sdsType="primary"
+        onClose={setBottomBannerLastClosedTime}
+      >
+        <BannerContent surveyLink={surveyLink} />
+      </StyledBanner>
+    </StyledBottomBannerWrapper>
+  );
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(BottomBanner);
+export default connect(mapStateToProps, mapDispatchToProps)(memo(BottomBanner));
