0b1a1fa2cc1c3d9c5d3700ad4d31735394dc6ac3
kaloster <rkalo@contractor.chanzuckerberg.com>
2024-08-22 11:46:10 -0400

    chore: add ReadableStream to globals

--- client/__tests__/setupMissingGlobals.ts
+++ client/__tests__/setupMissingGlobals.ts
@@ -5,6 +5,10 @@ the jest test environment).
 
 import { TextDecoder, TextEncoder } from "util";
 
+import { ReadableStream } from "node:stream/web";
+
 // @ts-expect-error ts-migrate(2322) FIXME: Type 'typeof TextDecoder' is not assignable to typ... Remove this comment to see the full error message
 global.TextDecoder = TextDecoder;
 global.TextEncoder = TextEncoder;
+// @ts-expect-error ts-migrate(2322) FIXME: Type 'typeof ReadableStream' is not assignable to typ... Remove this comment to see the full error message
+global.ReadableStream = ReadableStream;
