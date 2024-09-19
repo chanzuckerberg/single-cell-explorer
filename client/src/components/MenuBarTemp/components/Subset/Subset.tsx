2bc91f9fa6986de38797fb75971c81223d8c1293
Suzette McCanny <smccanny@deloitte.ca>
2024-09-19 16:13:57 -0400

    more menubar.css

--- client/src/components/MenuBarTemp/components/Subset/Subset.tsx
+++ client/src/components/MenuBarTemp/components/Subset/Subset.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { AnchorButton, ButtonGroup, Tooltip } from "@blueprintjs/core";
 // @ts-expect-error ts-migrate(2307) FIXME: Cannot find module './menubar.css' or its correspo... Remove this comment to see the full error message
-import styles from "./menubar.css";
-import * as globals from "../../../../globals";
+import styles from "../../menubar.css";
+import * as globals from "~/globals";
 
 interface SubsetProps {
   subsetPossible: boolean;
