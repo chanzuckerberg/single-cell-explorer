684c12e338271e5b043e0d895c861215af49eb32
atarashansky <atarashansky@chanzuckerberg.com>
2025-04-25 12:09:14 -0700

    feat: update url routing for w dataroot

--- server/app/app.py
+++ server/app/app.py
@@ -197,20 +197,28 @@ class Server:
 
         for dataroot_dict in app_config.server__multi_dataset__dataroots.values():
             url_dataroot = dataroot_dict["base_url"]
-            self.app.add_url_rule(
-                f"/{url_dataroot}/<string:dataset>/",
-                f"dataset_index_{url_dataroot}/",
-                lambda dataset, url_dataroot=url_dataroot: dataset_index(url_dataroot, dataset),
-                methods=["GET"],
-            )
-            self.app.add_url_rule(
-                f"/{url_dataroot}/{CELLGUIDE_CXG_KEY_NAME}/<path:dataset>.cxg/",
-                f"dataset_index_{url_dataroot}_cellguide_cxgs/",
-                lambda dataset, url_dataroot=url_dataroot: dataset_index(
-                    url_dataroot, f"{CELLGUIDE_CXG_KEY_NAME}/{dataset}.cxg"
-                ),
-                methods=["GET"],
-            )
+            if url_dataroot == "w":
+                self.app.add_url_rule(
+                    f"/{url_dataroot}/<path:dataset>/",
+                    f"dataset_index_{url_dataroot}/",
+                    lambda dataset, url_dataroot=url_dataroot: dataset_index(url_dataroot, dataset),
+                    methods=["GET"],
+                )
+            else:
+                self.app.add_url_rule(
+                    f"/{url_dataroot}/<string:dataset>/",
+                    f"dataset_index_{url_dataroot}/",
+                    lambda dataset, url_dataroot=url_dataroot: dataset_index(url_dataroot, dataset),
+                    methods=["GET"],
+                )
+                self.app.add_url_rule(
+                    f"/{url_dataroot}/{CELLGUIDE_CXG_KEY_NAME}/<path:dataset>.cxg/",
+                    f"dataset_index_{url_dataroot}_cellguide_cxgs/",
+                    lambda dataset, url_dataroot=url_dataroot: dataset_index(
+                        url_dataroot, f"{CELLGUIDE_CXG_KEY_NAME}/{dataset}.cxg"
+                    ),
+                    methods=["GET"],
+                )
 
         self.app.app_config = app_config
 
