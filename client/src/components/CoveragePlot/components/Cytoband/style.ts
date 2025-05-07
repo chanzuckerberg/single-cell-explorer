fe56c24b80318684a4272749e5af43a5807a7b38
Suzette McCanny <smccanny@deloitte.ca>
2025-05-07 15:31:37 -0400

    feat: Add Cytoband to Chromatin Viz (#1189)

---
+++ client/src/components/CoveragePlot/components/Cytoband/style.ts
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+
+export const CytobandWrapper = styled.div`
+  .gneg {
+    fill: #f3f3f3;
+  }
+  .gpos25 {
+    fill: #d9d9d9;
+  }
+  .gpos50 {
+    fill: #c5c5c5;
+  }
+  .gpos75 {
+    fill: #b2b2b2;
+  }
+  .gpos100 {
+    fill: #969696;
+  }
+  .acen {
+    fill: black;
+    stroke: black;
+  }
+  .gvar {
+    fill: lightpink;
+  }
+`;
