f0b76802175c3bddde7f79b2b106b4f97269de59
Jeremy <jeremyasuncion808@gmail.com>
2025-05-12 08:18:01 -0700

    chromatin view button

--- client/src/common/components/Embedding/style.ts
+++ client/src/common/components/Embedding/style.ts
@@ -1,12 +1,24 @@
-import { Button } from "@blueprintjs/core";
-import styled from "@emotion/styled";
+import { Button } from '@blueprintjs/core'
+import { CommonThemeProps } from '@czi-sds/components'
+import styled from '@emotion/styled'
+import { primary400 } from 'util/theme'
 
 export const ImageToggleWrapper = styled.span`
   margin-left: 8px;
   display: flex;
-`;
+`
 
 export const ImageDropdownButton = styled(Button)`
   /* (thuang): Make the caret button narrower */
   min-width: 10px;
-`;
+`
+
+interface ChromatinIconContainerProps extends CommonThemeProps {
+  active: boolean
+}
+
+export const ChromatinIconContainer = styled.div<ChromatinIconContainerProps>`
+  .bp5-icon svg {
+    color: ${props => props.active ? primary400 : 'unset'};
+  }
+`
