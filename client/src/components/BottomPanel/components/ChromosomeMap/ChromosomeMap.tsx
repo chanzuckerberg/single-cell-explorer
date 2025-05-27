4eb2b5d2f3f73319a3f719aa5153dd1f02d7be39
Suzette McCanny <smccanny@deloitte.ca>
2025-05-27 15:15:24 -0400

    get rdev to update

--- client/src/components/BottomPanel/components/ChromosomeMap/ChromosomeMap.tsx
+++ client/src/components/BottomPanel/components/ChromosomeMap/ChromosomeMap.tsx
@@ -172,6 +172,7 @@ export const ChromosomeMap = () => {
   }
   return (
     <>
+      <p>Hello</p>
       <Cytoband chromosomeId={chromosome} />
       <CoverageAtScale ref={scrollContainerRef}>
         <ScaleBar
