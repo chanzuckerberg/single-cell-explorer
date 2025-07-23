b7d5c3f453401b486b04f3ae4d6e253d9f6b042c
Ronen <kaloster@users.noreply.github.com>
2025-07-23 18:54:33 -0400

    fix: Grab organism_ontology_term_id from config (#1244)

--- client/src/common/hooks/useChromatinViewerSelectedGene.tsx
+++ client/src/common/hooks/useChromatinViewerSelectedGene.tsx
@@ -5,6 +5,8 @@ import React, {
   useEffect,
   ReactNode,
 } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../reducers";
 
 interface ChromatinViewerContextType {
   selectedGene: string;
@@ -26,13 +28,50 @@ export function ChromatinViewerProvider({
   children,
   initialSelectedGene,
 }: ChromatinViewerProviderProps) {
-  const DEFAULT_GENE = "MYC";
   const DEFAULT_GENOME_VERSION = "hg38";
+  const DEFAULT_GENE = "MYC";
+
+  // Get organism ontology term ID from config to determine genome version
+  const config = useSelector((state: RootState) => state.config);
+  const organismOntologyTermId =
+    config?.corpora_props?.organism_ontology_term_id;
+
+  // Map organism ontology term ID to genome version
+  const getGenomeVersionFromOrganism = (ontologyTermId?: string): string => {
+    switch (ontologyTermId) {
+      case "NCBITaxon:9606": // Human
+        return "hg38";
+      case "NCBITaxon:10090": // Mouse
+        return "mm39";
+      default:
+        return DEFAULT_GENOME_VERSION;
+    }
+  };
+
+  // Map organism ontology term ID to default gene
+  const getDefaultGeneFromOrganism = (ontologyTermId?: string): string => {
+    switch (ontologyTermId) {
+      case "NCBITaxon:9606":
+        return "MYC";
+      case "NCBITaxon:10090":
+        return "Vip";
+      default:
+        return DEFAULT_GENE;
+    }
+  };
+
+  const detectedGenomeVersion = getGenomeVersionFromOrganism(
+    organismOntologyTermId
+  );
+  const detectedDefaultGene = getDefaultGeneFromOrganism(
+    organismOntologyTermId
+  );
+
   const [selectedGene, setSelectedGene] = useState<string>(
-    initialSelectedGene || DEFAULT_GENE
+    initialSelectedGene || detectedDefaultGene
   );
   const [genomeVersion, setGenomeVersionState] = useState<string>(
-    DEFAULT_GENOME_VERSION
+    detectedGenomeVersion
   );
 
   // Update selected gene when initialSelectedGene changes (from Redux)
@@ -41,6 +80,22 @@ export function ChromatinViewerProvider({
       setSelectedGene(initialSelectedGene);
     }
   }, [initialSelectedGene]);
+
+  // Update genome version when organism ontology term ID changes
+  useEffect(() => {
+    const newGenomeVersion = getGenomeVersionFromOrganism(
+      organismOntologyTermId
+    );
+    setGenomeVersionState(newGenomeVersion);
+  }, [organismOntologyTermId]);
+
+  // Update default gene when organism ontology term ID changes (only if no initial gene provided)
+  useEffect(() => {
+    if (!initialSelectedGene) {
+      const newDefaultGene = getDefaultGeneFromOrganism(organismOntologyTermId);
+      setSelectedGene(newDefaultGene);
+    }
+  }, [organismOntologyTermId, initialSelectedGene]);
 
   const setGenomeVersion = (version: string) => {
     setGenomeVersionState(version);
