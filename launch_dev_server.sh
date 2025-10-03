2ffa0b97d168d480b392600e305df78012c53717
atarashansky <atarashansky@chanzuckerberg.com>
2025-10-03 12:19:25 -0700

    update

--- launch_dev_server.sh
+++ launch_dev_server.sh
@@ -8,13 +8,5 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 export PYTHONPATH=${PROJECT_ROOT}  # permits module discovery when run from somewhere other than top level dir
 
-# Use multi-dataset mode with both /d/ and /w/ dataroots for testing authorization
-# Both point to the same example dataset directory
-# Usage: ./launch_dev_server.sh example-dataset
-DATAROOT_PATH=${1:-example-dataset}
-
-PROJECT_ROOT=${PROJECT_ROOT} python -m server.cli.launch \
-    --multi-dataroots "d:${DATAROOT_PATH}" \
-    --multi-dataroots "w:${DATAROOT_PATH}" \
-    --debug
+PROJECT_ROOT=${PROJECT_ROOT} python -m server.cli.launch -d $@
 
