f83025034c53ca9a25f39ea6bbbabcd60fcc0d5c
Erin Hoops <erin.hoops+gh@chanzuckerberg.com>
2025-11-18 11:31:03 -0800

    linting

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -8,8 +8,6 @@ import actions from "actions";
 import { RootState, AppDispatch } from "reducers";
 import Controls from "common/components/Controls/Controls";
 import { theme } from "util/theme";
-import { getFeatureFlag } from "util/featureFlags/featureFlags";
-import { FEATURES } from "util/featureFlags/features";
 import DatasetSelector from "../DatasetSelector/DatasetSelector";
 import DiffexNotice from "../DiffexNotice/DiffexNotice";
 import Container from "../framework/container";
